af036cd7a6953e4393af560d565dadca82fc730d
Pascal Essiembre <pascal.essiembre@norconex.com>
2014-04-14 14:51:50 -0400

    Documentation update.

--- norconex-importer/src/main/java/com/norconex/importer/tagger/impl/HierarchyTagger.java
+++ norconex-importer/src/main/java/com/norconex/importer/tagger/impl/HierarchyTagger.java
@@ -62,7 +62,7 @@ import com.norconex.importer.tagger.IDocumentTagger;
  * XML configuration usage:
  * </p>
  * <pre>
- *  &lt;tagger class="com.norconex.importer.tagger.impl.HierarchycalTagger"&gt;
+ *  &lt;tagger class="com.norconex.importer.tagger.impl.HierarchyTagger"&gt;
  *      &lt;hierarchy fromField="(from field)" toField="(to field)" 
  *                 fromSeparator="(original separator)" toSeparator="(new separator)"
  *                 overwrite="[false|true]" /&gt
