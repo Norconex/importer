ae5daf484984b97212a10930fef8e47f0a9c404d
Pascal Essiembre <pascal.essiembre@norconex.com>
2015-06-09 21:45:37 -0400

    Added comments on how to change the system temp directory for temp file creation.

--- norconex-importer/src/main/scripts/importer.bat
+++ norconex-importer/src/main/scripts/importer.bat
@@ -1,4 +1,12 @@
 @echo off
 cd %~dp0
 
+REM Third-party libraries sometimes have to create and write to temporary files.
+REM By default those are created in your system "temp" folder 
+REM (usually defined under %TEMP% variable in Windows).
+REM To change the temporary location those libraries will use, add the
+REM following to the java command below (replacing the path):
+REM
+REM     -Djava.io.tmpdir="C:\temp"
+
 java -Dlog4j.configuration="file:///%ROOT_DIR%classes/log4j.properties" -Dfile.encoding=UTF8 -cp "./lib/*;./classes" com.norconex.importer.Importer %*
--- norconex-importer/src/main/scripts/importer.sh
+++ norconex-importer/src/main/scripts/importer.sh
@@ -2,4 +2,12 @@
 
 cd $(dirname $0)
 
+# Third-party libraries sometimes have to create and write to temporary files.
+# By default those are created in your system "temp" folder
+# (usually /tmp/ under Linux/Unix).
+# To change the temporary location those libraries will use, add the
+# following to the java command below (replacing the path):
+#
+#     -Djava.io.tmpdir=/path/to/tmp
+
 java -Dlog4j.configuration="file:${ROOT_DIR}/classes/log4j.properties" -Dfile.encoding=UTF8 -cp "./lib/*:./classes" com.norconex.importer.Importer "$@"
