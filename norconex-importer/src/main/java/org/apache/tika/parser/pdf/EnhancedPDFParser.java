9c5a724d4ca74aafc1cd644f859f77542f9187b6
Pascal Essiembre <pascal.essiembre@norconex.com>
2015-08-05 23:17:08 -0400

    Fixed latest PDFBox snapshot failing pdf parsing.

--- norconex-importer/src/main/java/org/apache/tika/parser/pdf/EnhancedPDFParser.java
+++ norconex-importer/src/main/java/org/apache/tika/parser/pdf/EnhancedPDFParser.java
@@ -52,7 +52,6 @@ import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDXFAResource;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.extractor.EmbeddedDocumentExtractor;
-import org.apache.tika.io.CloseShieldInputStream;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.PagedText;
@@ -120,25 +119,25 @@ public class EnhancedPDFParser extends AbstractParser {
     private static final Pattern PATTERN_STRIP_MARKUP = Pattern.compile("<.*?>",
             Pattern.DOTALL);
 
-    private boolean preserveMemory;
+//    private boolean preserveMemory;
     
     public Set<MediaType> getSupportedTypes(ParseContext context) {
         return SUPPORTED_TYPES;
     }
 
-    public boolean isPreserveMemory() {
-        return preserveMemory;
-    }
-    /**
-     * Sets whether memory should be preserved by using temp files to
-     * store the many PDF resources discovered.  This can drastically slow
-     * down parsing.
-     * @param preserveMemory <code>true</code> to preserve memory by using
-     *                       temp files
-     */
-    public void setPreserveMemory(boolean preserveMemory) {
-        this.preserveMemory = preserveMemory;
-    }
+//    public boolean isPreserveMemory() {
+//        return preserveMemory;
+//    }
+//    /**
+//     * Sets whether memory should be preserved by using temp files to
+//     * store the many PDF resources discovered.  This can drastically slow
+//     * down parsing.
+//     * @param preserveMemory <code>true</code> to preserve memory by using
+//     *                       temp files
+//     */
+//    public void setPreserveMemory(boolean preserveMemory) {
+//        this.preserveMemory = preserveMemory;
+//    }
 
     public void parse(
             InputStream stream, ContentHandler handler,
@@ -155,12 +154,24 @@ public class EnhancedPDFParser extends AbstractParser {
             // Decide which to do based on if we're reading from a file or not already
             TikaInputStream tstream = TikaInputStream.cast(stream);
             password = getPassword(metadata, context);
+
             
-            if (localConfig.getUseNonSequentialParser() == true) {
-                pdfDocument = PDDocument.load(new CloseShieldInputStream(tstream), password);
-            } else {
-                pdfDocument = PDDocument.load(new CloseShieldInputStream(tstream), preserveMemory);
-            }
+            // with preserve memory being true (using scratch file). it
+            // started to fail with snapshot version, so stream should always
+            // be CachedInputStream to avoid issue until PDFBox is more stable.
+            pdfDocument = PDDocument.load(tstream, password, false);//, preserveMemory);
+            
+//            long maxMemory = 10 * 1024 * 1024; // 10 MB
+//            if (stream instanceof CachedInputStream) {
+//                maxMemory = ((CachedInputStream) stream).getMemCacheSize();
+//            }
+//            pdfDocument = PDDocument.load(tstream, password, MemoryUsageSetting.setupMixed(maxMemory));
+
+//            if (localConfig.getUseNonSequentialParser() == true) {
+//                pdfDocument = PDDocument.load(new CloseShieldInputStream(tstream), password);
+//            } else {
+//                pdfDocument = PDDocument.load(new CloseShieldInputStream(tstream), preserveMemory);
+//            }
             
             metadata.set("pdf:encrypted", Boolean.toString(pdfDocument.isEncrypted()));
 
