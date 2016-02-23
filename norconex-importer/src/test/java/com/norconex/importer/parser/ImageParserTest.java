a070e8a6421cd7cc5a06f64b88668fb3c865a024
Pascal Essiembre <pascal.essiembre@norconex.com>
2016-02-23 01:04:13 -0500

    Added Jempbox 1.8.11 Maven dependency back as it is required for parsing of jpeg files containing XMP metadata. Created images unit tests.

---
+++ norconex-importer/src/test/java/com/norconex/importer/parser/ImageParserTest.java
@@ -0,0 +1,71 @@
+/* Copyright 2016 Norconex Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.norconex.importer.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.norconex.importer.doc.ImporterMetadata;
+import com.norconex.importer.response.ImporterResponse;
+
+public class ImageParserTest extends AbstractParserTest {
+
+    @Test
+    public void testBMP() throws Exception {
+        testParsing("image/x-ms-bmp", "bmp");
+    }
+    @Test
+    public void testGIF() throws Exception {
+        testParsing("image/gif", "gif");
+    }
+    @Test
+    public void testJPG() throws Exception {
+        testParsing("image/jpeg", "jpg");
+    }
+    @Test
+    public void testJPG_XMP() throws Exception {
+        // JPEG with XMP metadata.  Can be dealt with, with a tool such
+        // as http://www.exiv2.org
+        // Currently parsed by Tika using Jempbox
+        ImporterResponse[] responses = 
+                testParsing("/parser/image/importer-xmp.jpg",
+                        "image/jpeg", ".*Importer.*", "jpg", "Image");
+        ImporterMetadata meta = responses[0].getDocument().getMetadata();
+        Assert.assertEquals("Could not find XMP metadata dc:subject with "
+                + "expected value \"XML Parsing\".",
+                "XMP Parsing", meta.getString("dc:subject"));
+        System.out.println(meta);
+    }
+    @Test
+    public void testPNG() throws Exception {
+        testParsing("image/png", "png");
+    }
+    @Test
+    public void testPSD() throws Exception {
+        testParsing("/parser/image/importer.psd",
+                "image/vnd.adobe.photoshop", ".*", "psd", "Image");        
+    }
+    @Test
+    public void testTIF() throws Exception {
+        testParsing("/parser/image/importer.tif",
+                "image/tiff", ".*", "tiff", "Image");        
+    }
+    
+    private void testParsing(String contentType, String extension)
+            throws Exception {
+        testParsing("/parser/image/importer." + extension,
+                contentType, ".*Importer.*", extension, "Image");
+    }
+}
