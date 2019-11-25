947bd62a7a3166bb03372938c40702cd8c4dc14f
Pascal Essiembre <pascal.essiembre@norconex.com>
2019-11-24 22:35:52 -0500

    Added a split tagger unit test.

--- norconex-importer/src/test/java/com/norconex/importer/handler/tagger/impl/SplitTaggerTest.java
+++ norconex-importer/src/test/java/com/norconex/importer/handler/tagger/impl/SplitTaggerTest.java
@@ -1,4 +1,4 @@
-/* Copyright 2010-2014 Norconex Inc.
+/* Copyright 2010-2019 Norconex Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ import org.junit.Test;
 import com.norconex.commons.lang.config.XMLConfigurationUtil;
 import com.norconex.importer.doc.ImporterMetadata;
 import com.norconex.importer.handler.ImporterHandlerException;
-import com.norconex.importer.handler.tagger.impl.SplitTagger;
 
 public class SplitTaggerTest {
 
@@ -39,20 +38,20 @@ public class SplitTaggerTest {
         XMLConfigurationUtil.assertWriteRead(tagger);
     }
 
-    
+
     @Test
     public void testRegularSplit() throws ImporterHandlerException {
         ImporterMetadata meta = new ImporterMetadata();
-        meta.addString("metaToSplitSameField", "Joe, Jack, William, Avrel"); 
-        meta.addString("metaNoSplitSameField", "Joe Jack William Avrel"); 
-        meta.addString("metaToSplitNewField", "Joe, Jack, William, Avrel"); 
-        meta.addString("metaNoSplitNewField", "Joe Jack William Avrel"); 
-        meta.addString("metaMultiSameField", 
-                "Joe, Jack", "William, Avrel"); 
-        meta.addString("metaMultiNewField", 
-                "Joe, Jack", "William, Avrel"); 
-        
-        
+        meta.addString("metaToSplitSameField", "Joe, Jack, William, Avrel");
+        meta.addString("metaNoSplitSameField", "Joe Jack William Avrel");
+        meta.addString("metaToSplitNewField", "Joe, Jack, William, Avrel");
+        meta.addString("metaNoSplitNewField", "Joe Jack William Avrel");
+        meta.addString("metaMultiSameField",
+                "Joe, Jack", "William, Avrel");
+        meta.addString("metaMultiNewField",
+                "Joe, Jack", "William, Avrel");
+
+
         SplitTagger tagger = new SplitTagger();
         tagger.addSplit("metaToSplitSameField", ", ", false);
         tagger.addSplit("metaNoSplitSameField", ", ", false);
@@ -64,10 +63,10 @@ public class SplitTaggerTest {
         tagger.tagDocument("n/a", null, meta, true);
 
         List<String> toSplitExpect = Arrays.asList(
-                "Joe", "Jack", "William", "Avrel"); 
+                "Joe", "Jack", "William", "Avrel");
         List<String> noSplitExpect = Arrays.asList(
-                "Joe Jack William Avrel"); 
-        
+                "Joe Jack William Avrel");
+
         Assert.assertEquals(
                 toSplitExpect, meta.getStrings("metaToSplitSameField"));
         Assert.assertEquals(
@@ -84,25 +83,38 @@ public class SplitTaggerTest {
         Assert.assertEquals(
                 noSplitExpect, meta.getStrings("metaNoSplitSameField"));
     }
-    
+
     @Test
     public void testRegexSplit() throws ImporterHandlerException {
         ImporterMetadata meta = new ImporterMetadata();
-        meta.addString("path1", "/a/path/file.doc"); 
-        meta.addString("path2", "a, b,c d;e, f"); 
-        
+        meta.addString("path1", "/a/path/file.doc");
+        meta.addString("path2", "a, b,c d;e, f");
+
         SplitTagger tagger = new SplitTagger();
         tagger.addSplit("path1", "/", true);
         tagger.addSplit("path2", "file2", "[, ;]+", true);
-        
+
         tagger.tagDocument("n/a", null, meta, true);
 
         Assert.assertEquals(
-                Arrays.asList("a", "path", "file.doc"), 
+                Arrays.asList("a", "path", "file.doc"),
                 meta.getStrings("path1"));
         Assert.assertEquals(
-                Arrays.asList("a", "b", "c", "d", "e", "f"), 
+                Arrays.asList("a", "b", "c", "d", "e", "f"),
                 meta.getStrings("file2"));
     }
 
+    // Test for https://github.com/Norconex/importer/issues/97
+    @Test
+    public void testOverwritten() throws ImporterHandlerException {
+        ImporterMetadata meta = new ImporterMetadata();
+        meta.addString("test", "Joe, Jack, William, Avrel");
+
+        SplitTagger tagger = new SplitTagger();
+        tagger.addSplit("test", "\\s*,\\s*", true);
+
+        tagger.tagDocument("n/a", null, meta, true);
+
+        Assert.assertEquals(4, meta.getStrings("test").size());
+    }
 }
